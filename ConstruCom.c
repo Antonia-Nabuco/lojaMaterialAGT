258324467e078b1e54aed2c23a53a2d0785dfe1e
Estevão Tarifa <108072544+estevao-tarifa@users.noreply.github.com>
2025-10-13 14:11:49 -0300

    Fix input handling and improve prompts in ConstruCom1
    13-10-2025

--- ConstruCom.c
+++ ConstruCom.c
@@ -1,43 +1,45 @@
 #include <stdio.h>
+#include <string.h>
     int main(){
         char opcao;
         printf("\nOlá seja bem vindo ! \nO sistema CONSTRUCOM é uma aplicação de orçamento e compra de materiais de construção");
-        printf("\ndigite : \na) Ver Horarios \nb) Fazer Horcamento de materiais\n");
+        printf("\nDigite : \na) Ver Horarios \nb) Fazer orcamento de materiais\n");
         scanf(" %c",&opcao);
         if(opcao=='A' || opcao=='a'){
-            printf("segue o hoarios(blablanla terminar\n)");
+            printf("Segue os horarios: Segunda a Sexta, das 8h às 18h\n");
         } 
         
         if (opcao=='B' || opcao=='b'){
-            char name[20], email[40], phone[12],cpf[15],cnpj[20],type[02];
-            printf("\nSe Pessoa Fisica(digite PF)\nSe Pessoa Juridica(digite PJ)");
-            scanf("%c",&type);
-            if (type == "PF" || type == "pf" ){
-                printf("\nDigite o seu nome :\n");
-                scanf("%c",&name);
-                printf("\nDigite o seu email :\n");
-                scanf("%c",&email);
-                printf("\nDigite o seu telefone :\n");
-                scanf("%c",&phone);
-                printf("\nDigite o seu cpf :\n");
-                scanf("%c",&cpf);
-            }
-            if (type == "PJ" || type == "pj" ){
-                printf("\nDigite o nome da sua empresa :\n");
-                scanf("%c",&name);
-                printf("\nDigite o email da sua empres :\n");
-                scanf("%c",&email);
-                printf("\nDigite o seu telefone :\n");
-                scanf("%c",&phone);
-                printf("\nDigite o seu CNPJ da sua empres :\n");
-                scanf("%c",&cnpj);
-            }
-            else {printf("Desculpa, esta opção não exite");}
+            char name[20], email[40], phone[12], cpf[15], cnpj[20], type[3];
+            printf("\nSe Pessoa Fisica(digite PF)\nSe Pessoa Juridica(digite PJ): ");
+            scanf("%2s", type);
+            printf("\nDigite o seu nome :\n");
+            scanf("%19s", name);
+            printf("\nDigite o seu email :\n");
+            scanf("%39s", email);
+            printf("\nDigite o seu telefone :\n");
+            scanf("%11s", phone);
 
+            if (strcmp(type, "PF") == 0 || strcmp(type, "pf") == 0) {
+                printf("\nDigite o seu cpf :\n");
+                scanf("%14s", cpf);
+            }
+            else if (strcmp(type, "PJ") == 0 || strcmp(type, "pj") == 0) {
+                printf("\nDigite o nome da sua empresa :\n");
+                scanf("%19s", name);
+                printf("\nDigite o email da sua empresa :\n");
+                scanf("%39s", email);
+                printf("\nDigite o seu CNPJ da sua empresa :\n");
+                scanf("%19s", cnpj);
+            }
+            else {
+                printf("Desculpa, esta opção não existe\n");
+                return 0;
+            }
         }
         
         else{
-            printf("Desculpa, esta opção não exite");
+            printf("Desculpa, esta opção não existe\n");
         }
         
     }
